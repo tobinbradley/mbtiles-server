fcc3ca95577fd0612cb80f46a9f6c8e50d3a1698
tobinbradley <tobin.bradley@gmail.com>
2014-03-03 15:01:51 -0500

    added crontab instructions for starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# This would be better in upstart or systemd, but cron works and is a bit easier
+# edit crontab via crontab -e
+# Then stick this in:
+# @reboot /path/to/starter.sh
+# make sure that starter.sh is executable (chmod +x starter.sh)
+
 cd /opt/apps/tiles
 forever start server.js
